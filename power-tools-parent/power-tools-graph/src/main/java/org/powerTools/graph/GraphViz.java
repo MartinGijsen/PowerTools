47730a2322181f2c3bd06a00fed06a52515ea36e
Martin Gijsen <martin@deanalist.nl>
2013-02-04 11:27:52 +0100

    added/updated legal notice

--- power-tools-parent/power-tools-graph/src/main/java/org/powerTools/graph/GraphViz.java
+++ power-tools-parent/power-tools-graph/src/main/java/org/powerTools/graph/GraphViz.java
@@ -1,3 +1,21 @@
+/*	Copyright 2013 by Martin Gijsen (www.DeAnalist.nl)
+ *
+ *	This file is part of the PowerTools.
+ *
+ *	The PowerTools are free software: you can redistribute them and/or
+ *	modify them under the terms of the GNU Affero General Public License as
+ *	published by the Free Software Foundation, either version 3 of the License,
+ *	or (at your option) any later version.
+ *
+ *	The PowerTools are distributed in the hope that they will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with the PowerTools. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.powerTools.graph;
 
 import java.io.File;
