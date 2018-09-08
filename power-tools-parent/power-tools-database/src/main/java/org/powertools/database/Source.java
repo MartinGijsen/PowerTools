1ad082cc351f2c4c24bc8931164aa80e0fae4346
MartinGijsen <martin@deanalist.nl>
2018-09-08 18:33:23 +0200

    support join synonyms

--- power-tools-parent/power-tools-database/src/main/java/org/powertools/database/Source.java
+++ power-tools-parent/power-tools-database/src/main/java/org/powertools/database/Source.java
@@ -11,15 +11,31 @@ public abstract class Source {
         return new TablesForJoin ("JOIN", this, table);
     }
 
+    public TablesForJoin innerJoin (Table table) {
+        return new TablesForJoin ("INNER JOIN", this, table);
+    }
+
     public TablesForJoin leftJoin (Table table) {
         return new TablesForJoin ("LEFT JOIN", this, table);
+    }
+
+    public TablesForJoin leftOuterJoin (Table table) {
+        return new TablesForJoin ("LEFT OUTER JOIN", this, table);
     }
 
     public TablesForJoin rightJoin (Table table) {
         return new TablesForJoin ("RIGHT JOIN", this, table);
     }
 
+    public TablesForJoin rightOuterJoin (Table table) {
+        return new TablesForJoin ("RIGHT OUTER JOIN", this, table);
+    }
+
     public TablesForJoin fullJoin (Table table) {
         return new TablesForJoin ("FULL JOIN", this, table);
+    }
+
+    public TablesForJoin fullOuterJoin (Table table) {
+        return new TablesForJoin ("FULL OUTER JOIN", this, table);
     }
 }
